eca402b6fa65f381ae444409b717f9f4ab8b6c69
LimChaeyeon0629 <ssjm0629@gmail.com>
2026-04-02 20:10:32 +0900

    IfEx007.java

--- track003_java/java2026/src/com/the703/basic005_ex/IfEx007.java
+++ track003_java/java2026/src/com/the703/basic005_ex/IfEx007.java
@@ -5,18 +5,12 @@ import java.util.Scanner;
 public class IfEx007 {
 
 	public static void main(String[] args) {
-//		계산기
-//		1. 정수를 하나 입력해주세요 > 10
-//		2. 정수를 하나 입력해주세요 > 3
-//		3. 연산자를 입력해주세요(+,-,*,/) > +
-//		10+3=13
 
 		// 변수
 		Scanner sc = new Scanner(System.in);
-		int num1 = -1, num2 = -1;
-		double num3;
-		char ch;
-		// String s; 안 되는 이유: 문자열은 .equals();
+		int num1 = -1, num2 = -1, num3 = -1;
+		double num4 = -1;
+		char ch; // String s; 안 되는 이유 : 문자열은 .equals();
 		
 		// 입력
 		System.out.print("정수1 입력: ");
@@ -26,19 +20,33 @@ public class IfEx007 {
 		System.out.print("연산자 입력: ");
 		ch = sc.next().charAt(0);
 		
-		num3 = (double)num1 / num2;
-		
-		// 처릭
+		// 처리
 		if (ch == '+') {
-			System.out.printf("%d + %d" + " = " + "%d", num1, num2, num1+num2);
+			num3 = (num1 + num2);
 		} else if (ch == '-') {
-			System.out.printf("%d - %d" + " = " + "%d", num1, num2, num1-num2);
+			num3 = (num1 - num2);
 		} else if (ch == '*') {
-			System.out.printf("%d * %d" + " = " + "%d", num1, num2, num1*num2);
+			num3 = (num1 * num2);
 		} else if (ch == '/') {
-			System.out.printf("%d / %d" + " = " + "%.2f", num1, num2, num3);
+			num4 = (double)num1 / num2;
 		}
-	
+		
+		// 출력
+		if (ch == '+' || ch == '-' || ch == '*') {
+			System.out.printf("%d%c%d=%d", num1, ch, num2, num3);
+		} else if (ch == '/') {
+			System.out.printf("%d%c%d=%.2f", num1, ch, num2, num4);
+		}
 	}
 
 }
+
+//		if (ch == '+') {
+//			System.out.printf("%d + %d" + " = " + "%d", num1, num2, num1+num2);
+//		} else if (ch == '-') {
+//			System.out.printf("%d - %d" + " = " + "%d", num1, num2, num1-num2);
+//		} else if (ch == '*') {
+//			System.out.printf("%d * %d" + " = " + "%d", num1, num2, num1*num2);
+//		} else if (ch == '/') {
+//			System.out.printf("%d / %d" + " = " + "%.2f", num1, num2, num3);
+//		}
